a00c8d729f2946045bfd05267be32d516f8b7631
Prateek <prateek.singh@vvdntech.com>
2017-01-18 22:52:34 +0530

    added sliding images

--- backend/application/controllers/product.php
+++ backend/application/controllers/product.php
@@ -16,13 +16,13 @@ Class product extends CI_CONTROLLER {
         $this->load->model('general_model','gm',true);
         $this->load->model('product_model','pm',true);
         $this->load->model('category_model','cm',true);
-       
-       
+
+
 	}
 
 	public function add_product(){
 
-        
+
 		$data = file_get_contents("php://input");
 		$data = json_decode($data, TRUE);
 
@@ -37,10 +37,10 @@ Class product extends CI_CONTROLLER {
 			{
 				$this->gm->send_response(false,'Category_Not_Exists','','');
 			}
-			
+
 			$prod_where=array('product_name'=>$data['product_name'],'product_subcategory_id'=>$data['category_id']);
 			$flag=$this->pm->check_product_exists($prod_where);
-			
+
 			if($flag)
 			{
 				$this->gm->send_response(false,'Already_Exists','','');
@@ -50,7 +50,7 @@ Class product extends CI_CONTROLLER {
 				$prod_data=array('product_name'=>$data['product_name'],'product_subcategory_id'=>$data['category_id'],'product_category_id'=>$cat[0]['category_parent_id'],'product_added_on'=>time(),'product_updated_on'=>time());
 				$product_id=$this->pm->add_product($prod_data);
 				$this->gm->send_response(true,'Success','',$product_id);
-			}	
+			}
 		}
 		else
 		{
@@ -61,7 +61,7 @@ Class product extends CI_CONTROLLER {
 
 		public function list_product(){
 
-        
+
 		$data = file_get_contents("php://input");
 		$data = json_decode($data, TRUE);
 
@@ -69,7 +69,7 @@ Class product extends CI_CONTROLLER {
 		{
 			$cat_where=array('category_parent_id'=>$data['category_id']);
 			$cat_data=$this->cm->get_category($cat_where);
-			
+
 			if(sizeof($cat_data)==0)
 			{
 				$this->gm->send_response(false,'No_Category_Found','','');
@@ -77,11 +77,31 @@ Class product extends CI_CONTROLLER {
 			else
 			{
 				$this->gm->send_response(true,'Category_List','',$cat_data);
-			}	
+			}
 		}
 		else
 		{
 			$this->gm->send_response(false,'Empty_Field','','');
 		}
 	}
+
+
+
+  /********************************************************************************
+  * * Function            : product_details
+  * * Description         : product details according to product id
+  * * Input Parameters    : product_id,quantity
+  * * Return Values       :  true or false(JSON)
+  * ****************************************************************************** */
+
+  public function get_product_details($product_id){
+
+    //check empty fields
+    if (empty($product_id)) {
+      $this->gm->send_response(false,'Empty_Field','','Product');
+    }
+
+    $where_product = array('' => , );
+    $response=$this->pm->select_product()
+  }
 }
